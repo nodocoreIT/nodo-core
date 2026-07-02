8ec05434481c137d301859531d74b13e245e818c
Ramiro Toulemonde <nodocore.lp@gmail.com>
2026-07-02 09:09:38 -0300

    chore: elimina future_nodes/nodo-clinica

--- future_nodes/nodo-clinica/src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { isLocalMode } from "@/lib/clinic/config";
-
-export default function AuthLoginRedirect() {
-  if (isLocalMode()) {
-    redirect("/login");
-  }
-  redirect("/login");
-}
--- future_nodes/nodo-clinica/src/app/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LoginPortal } from "@/components/auth/login-portal";
-
-export default function LoginPage() {
-  return <LoginPortal />;
-}
